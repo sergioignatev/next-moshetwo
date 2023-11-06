346af3c268202cfc2ac836e25cd292417d14a98c
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-06 20:17:25 +0300

    j

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-   <h1 className='text-center text-[blue] text-[48px]'>MOSHE@ Test Two</h1>
+   <h1 className='text-center text-[pink] text-[48px]'>MOSHE@ Test Two</h1>
   )
 }
